990a20016aa31f028af59b6f5d2d0660d5ef9e4b
YarikMamykin <ymamykin@gmail.com>
2020-06-17 03:11:31 +0300

    Add example of specifying colors

--- src/xlib/X11_Window.cpp
+++ src/xlib/X11_Window.cpp
@@ -8,9 +8,13 @@ namespace xlib {
       , msg("Hello, World!") 
       , graphical_context(DefaultGC(x_display.display, screen)) {
 
+        long int border_color = 255L;
+        border_color = (border_color << 16);
+        long int background_color = 255L;
+        background_color = (background_color << 8);
         // create window
-        window = XCreateSimpleWindow(x_display.display, RootWindow(x_display.display, screen), 10, 10, 200, 200, 1,
-            BlackPixel(x_display.display, screen), WhitePixel(x_display.display, screen));
+        window = XCreateSimpleWindow(x_display.display, RootWindow(x_display.display, screen), 10, 10, 200, 200, 10,
+            border_color, background_color);
       }
 
   X11_Window::~X11_Window() {
@@ -23,6 +27,8 @@ namespace xlib {
   }
 
   void X11_Window::expose() {
+    long font_color = 255L << 16;
+    XSetForeground(x_display.display, graphical_context, font_color);
     XFillRectangle(x_display.display, window, graphical_context, 20, 200, 200, 30);
     XDrawString(x_display.display, window, graphical_context, 50, 50, msg.c_str(), msg.length());
   }
