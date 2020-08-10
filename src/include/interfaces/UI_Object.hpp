56caba6781368c345e25ed3189879c846ca0b563
YarikMamykin <ymamykin@gmail.com>
2020-08-11 01:10:17 +0300

    Add convertion of View and UI object to EventHandler

--- src/include/interfaces/UI_Object.hpp
+++ src/include/interfaces/UI_Object.hpp
@@ -2,14 +2,15 @@
 #define SRC_INCLUDE_INTERFACES_UI_OBJECT_HPP
 
 #include "XHeaders.hpp"
+#include <memory>
 
 namespace ui {
 
   class UI_Object {
     public:
-      virtual void handle_key_press(const KeySym&& key_sym) = 0;
-      virtual void handle_button_press(const unsigned int& button) = 0;
-      virtual void handle_mouse_position(const int& x, const int& y) = 0;
+      template <class EventHandlerType> static EventHandlerType* as_event_handler(std::shared_ptr<UI_Object> object) {
+        return dynamic_cast<EventHandlerType*>(object.get());
+      }
       virtual ~UI_Object() {};
   };
 
--- src/include/interfaces/View.hpp
+++ src/include/interfaces/View.hpp
@@ -1,13 +1,18 @@
 #ifndef SRC_INCLUDE_INTERFACES_VIEWS_HPP
 #define SRC_INCLUDE_INTERFACES_VIEWS_HPP
 
+#include <memory>
+
 namespace views {
 
   class View {
     public:
-    virtual void activate() = 0;
-    virtual void deactivate() = 0;
-    virtual ~View() {};
+      virtual void activate() = 0;
+      virtual void deactivate() = 0;
+      template <class EventHandlerType> static EventHandlerType* as_event_handler(std::shared_ptr<View> view) {
+        return dynamic_cast<EventHandlerType*>(view.get());
+      }
+      virtual ~View() {};
   };
 }
 
