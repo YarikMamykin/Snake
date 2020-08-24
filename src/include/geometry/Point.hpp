0dc69863e0aecc323d2280beba31f9dc0902d089
YarikMamykin <ymamykin@luxoft.com>
2020-08-24 18:39:06 +0300

    Add Point and Triangle classes

---
+++ src/include/geometry/Point.hpp
@@ -0,0 +1,11 @@
+#ifndef SRC_INCLUDE_GEOMETRY_POINT_HPP
+#define SRC_INCLUDE_GEOMETRY_POINT_HPP
+
+namespace geometry {
+  struct Point {
+    int x,y;
+  };
+}
+
+
+#endif /* SRC_INCLUDE_GEOMETRY_POINT_HPP */
---
+++ src/include/geometry/Triangle.hpp
@@ -0,0 +1,12 @@
+#ifndef SRC_INCLUDE_GEOMETRY_TRIANGLE_HPP
+#define SRC_INCLUDE_GEOMETRY_TRIANGLE_HPP
+
+#include "Point.hpp"
+
+namespace geometry {
+  struct Triangle {
+    Point points[3];
+  };
+}
+
+#endif /* SRC_INCLUDE_GEOMETRY_TRIANGLE_HPP */
