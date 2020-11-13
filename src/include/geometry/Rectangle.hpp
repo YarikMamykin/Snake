5bd87360240451b7272ed03abc7d3664fbda7907
YarikMamykin <ymamykin@gmail.com>
2020-11-13 02:06:54 +0200

    Add belongs_to method for Rectangle

--- src/include/geometry/Rectangle.hpp
+++ src/include/geometry/Rectangle.hpp
@@ -27,6 +27,21 @@ namespace geometry {
     Point top_left() const {
       return { .x = x, .y = y };
     }
+
+    bool belongs_to(const Rectangle& rect) const {
+      const auto&& tr = top_right();
+      const auto&& tl = top_left();
+      const auto&& bl = bottom_left();
+      const auto&& br = bottom_right();
+
+      bool result = true;
+      for(auto& point : { tr, tl, bl, br }) {
+        result &= (point.x >= rect.x && point.x <= rect.x + rect.width 
+            && point.y >= rect.y && point.y <= rect.y + rect.height);
+      }
+
+      return result;
+    }
   };
 }
 #endif /* SRC_INCLUDE_GEOMETRY_RECTANGLE_HPP */
