a497380f3cc57e28ba7faf49fdac1ecd6f7e64f0
YarikMamykin <ymamykin@gmail.com>
2020-12-12 10:31:12 +0200

    Implement == operator for Point

--- src/include/geometry/Point.hpp
+++ src/include/geometry/Point.hpp
@@ -12,6 +12,10 @@ namespace geometry {
     friend bool operator > (const Point& a, const Point& b) {
       return (a.x > b.x) && (a.y > b.y);
     }
+
+    friend bool operator == (const Point& a, const Point& b) {
+      return (a.x == b.x) && (a.y == b.y);
+    }
   };
 }
 
