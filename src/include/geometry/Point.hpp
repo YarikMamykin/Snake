ebf7086b4528fa3f3df47aa8e751f89ae480aac1
YarikMamykin <ymamykin@gmail.com>
2020-12-17 18:28:36 +0200

    Add >= operator for Point

--- src/include/geometry/Point.hpp
+++ src/include/geometry/Point.hpp
@@ -16,6 +16,12 @@ namespace geometry {
     friend bool operator == (const Point& a, const Point& b) {
       return (a.x == b.x) && (a.y == b.y);
     }
+
+    friend bool operator >= (const Point& a, const Point& b) {
+      bool equal = (a.x == b.x) && (a.y == b.y);
+      bool greater = a > b;
+      return equal || greater;
+    }
   };
 }
 
