aed7719c88ee699f2dfa7e0ac9f06feffab093be
YarikMamykin <ymamykin@gmail.com>
2020-12-26 18:12:59 +0200

    Adapt X11_TextLabel usage with TextLabel abstraction

--- src/include/xlib/X11_TextLabel.hpp
+++ src/include/xlib/X11_TextLabel.hpp
@@ -3,74 +3,27 @@
 
 #include "X11_Window.hpp"
 #include "Rectangle.hpp"
+#include "TextLabel.hpp"
 #include <string>
 
 namespace xlib {
 
-  class ITextLabel {
+  class X11_TextLabel : public abstractions::ui::TextLabel { 
     public:
-      virtual void show() = 0;
-      virtual void hide() = 0;
-      virtual void show_frame() = 0;
-      virtual void hide_frame() = 0;
-      virtual void set_text(const std::string& text) = 0;
-      virtual const std::string get_text() = 0;
-      virtual void set_background_color(unsigned long color) = 0;
-      virtual void set_frame_color(unsigned long color) = 0;
-      virtual void set_text_color(unsigned long color) = 0;
-      virtual void set_focused(bool focus) = 0;
-      virtual bool focused() const = 0;
-      virtual bool hovered_by_mouse(const int& x, const int& y) const = 0;
-      virtual void set_frame(const int& x, const int& y, const unsigned int& width, const unsigned int& height) = 0;
-      virtual const int get_x() const = 0;
-      virtual const int get_y() const = 0;
-      virtual const unsigned int get_width() const = 0;
-      virtual const unsigned int get_height() const = 0;
-      virtual void move(const int& x, const int& y) = 0;
-      virtual void set_position(const int& x, const int& y) = 0;
-
-      virtual ~ITextLabel() {};
-  };
-
-  class X11_TextLabel : public ITextLabel { 
-    public:
-      struct ColorScheme {
-        unsigned long background;
-        unsigned long frame;
-        unsigned long text;
-
-        ColorScheme() = default;
-        ColorScheme(const ColorScheme&) = default;
-      };
-
       static const unsigned int left_text_margin = 10U;
       static const unsigned int top_text_margin = 10U;
       static const unsigned int frame_weight = 3U;
 
     public:
       X11_TextLabel(X11_Window* parent_window);
-      X11_TextLabel(const std::string& text, const geometry::Rectangle& frame, const ColorScheme& color_scheme, X11_Window* parent_window);
+      X11_TextLabel(const std::string& text, 
+                    const geometry::Rectangle& frame, 
+                    const abstractions::ui::COLOR_SCHEME_TYPE& color_scheme, 
+                    X11_Window* parent_window);
       ~X11_TextLabel();
 
-      void show() override;
-      void hide() override;
-      void show_frame() override;
-      void hide_frame() override;
-      void set_text(const std::string& text) override;
-      const std::string get_text() override;
-      void set_background_color(unsigned long color) override;
-      void set_frame_color(unsigned long color) override;
-      void set_text_color(unsigned long color) override;
-      void set_focused(bool focus) override;
-      bool focused() const override;
-      bool hovered_by_mouse(const int& x, const int& y) const override;
-      void set_frame(const int& x, const int& y, const unsigned int& width, const unsigned int& height) override;
-      const int get_x() const override;
-      const int get_y() const override;
-      const unsigned int get_width() const override;
-      const unsigned int get_height() const override;
-      void move(const int& x, const int& y) override;
-      void set_position(const int& x, const int& y) override;
+      void show(bool show_flag) override;
+      void show_frame(bool show_flag) override;
 
     private:
       void update();
@@ -80,10 +33,6 @@ namespace xlib {
 
     private:
       X11_Window* parent_window;
-      ColorScheme color_scheme;
-      geometry::Rectangle frame;
-      std::string text;
-      bool focus;
   };
 }
 
--- src/xlib/X11_TextLabel.cpp
+++ src/xlib/X11_TextLabel.cpp
@@ -3,114 +3,54 @@
 namespace xlib {
 
   X11_TextLabel::X11_TextLabel(X11_Window* parent_window)
-  : parent_window(parent_window) 
-  , color_scheme() 
-  , frame() 
-  , text()
-  , focus(false) { }
+  : abstractions::ui::TextLabel()
+  , parent_window(parent_window) { }
 
-  X11_TextLabel::X11_TextLabel(const std::string& text, const geometry::Rectangle& frame, const ColorScheme& color_scheme, X11_Window* parent_window) 
-  : frame(frame)
-  , text(text)
-  , focus(false)
-  , color_scheme(color_scheme)
+  X11_TextLabel::X11_TextLabel(const std::string& text, 
+                               const geometry::Rectangle& frame, 
+                               const abstractions::ui::COLOR_SCHEME_TYPE& color_scheme, 
+                               X11_Window* parent_window) 
+  : abstractions::ui::TextLabel(text, frame, color_scheme)
   , parent_window(parent_window) {
-    this->frame = frame;
     update_frame();
   }
 
   X11_TextLabel::~X11_TextLabel() { }
 
-  void X11_TextLabel::show() {
-    update();
+  void X11_TextLabel::show(bool show_flag) {
+    if(show_flag) { 
+      update();
+    }
+
+    if(!show_flag) { 
+      auto& display = parent_window->x_display.display;
+      auto& graphical_context = parent_window->graphical_context;
+      auto& window = parent_window->window;
+
+      XSetForeground(display, graphical_context, this->color_scheme[ui::ColorSchemeID::BackgroundColor]);
+      XFillRectangle(display, window, graphical_context, this->frame.x, this->frame.y, this->frame.width, this->frame.height);
+    }
   }
 
-  void X11_TextLabel::hide() {
-    auto& display = parent_window->x_display.display;
-    auto& graphical_context = parent_window->graphical_context;
-    auto& window = parent_window->window;
+  void X11_TextLabel::show_frame(bool show_flag) {
+    if(show_flag) {
+      auto& display = parent_window->x_display.display;
+      auto& graphical_context = parent_window->graphical_context;
+      auto& window = parent_window->window;
 
-    XSetForeground(display, graphical_context, this->color_scheme.background);
-    XFillRectangle(display, window, graphical_context, this->frame.x, this->frame.y, this->frame.width, this->frame.height);
-  }
+      XSetLineAttributes(display, graphical_context, frame_weight,0,0,0);
+      XSetForeground(display, graphical_context, this->color_scheme[ui::ColorSchemeID::FrameColor]);
+      XDrawRectangle(display, window, graphical_context, this->frame.x, this->frame.y, this->frame.width, this->frame.height);
+    }
 
-  void X11_TextLabel::show_frame() {
-    auto& display = parent_window->x_display.display;
-    auto& graphical_context = parent_window->graphical_context;
-    auto& window = parent_window->window;
+    if(!show_flag) {
+      auto& display = parent_window->x_display.display;
+      auto& graphical_context = parent_window->graphical_context;
+      auto& window = parent_window->window;
 
-    XSetLineAttributes(display, graphical_context, frame_weight,0,0,0);
-    XSetForeground(display, graphical_context, this->color_scheme.frame);
-    XDrawRectangle(display, window, graphical_context, this->frame.x, this->frame.y, this->frame.width, this->frame.height);
-  }
-
-  void X11_TextLabel::hide_frame() {
-    auto& display = parent_window->x_display.display;
-    auto& graphical_context = parent_window->graphical_context;
-    auto& window = parent_window->window;
-
-    XSetForeground(display, graphical_context, this->color_scheme.background);
-    XDrawRectangle(display, window, graphical_context, this->frame.x, this->frame.y, this->frame.width, this->frame.height);
-  }
-
-  void X11_TextLabel::set_text(const std::string& text) {
-    this->text = text;
-  }
-
-  const std::string X11_TextLabel::get_text() {
-    return this->text;
-  }
-
-  void X11_TextLabel::set_background_color(unsigned long color) {
-    this->color_scheme.background = color;
-  }
-
-  void X11_TextLabel::set_frame_color(unsigned long color) {
-    this->color_scheme.frame = color;
-  }
-
-  void X11_TextLabel::set_text_color(unsigned long color) {
-    this->color_scheme.text = color;
-  }
-
-  void X11_TextLabel::set_focused(bool focus) {
-    this->focus = focus;
-  }
-
-  bool X11_TextLabel::focused() const {
-    return this->focus;
-  }
-
-  bool X11_TextLabel::hovered_by_mouse(const int &x, const int &y) const {
-    return this->frame.has_point({x,y});
-  }
-
-  void X11_TextLabel::set_frame(const int& x, const int& y, const unsigned int& width, const unsigned int& height) {
-    this->frame = {.x = x, .y = y, .width = width, .height = height};
-  }
-
-  const int X11_TextLabel::get_x() const {
-    return this->frame.x;
-  }
-
-  const int X11_TextLabel::get_y() const {
-    return this->frame.y;
-  }
-
-  const unsigned int X11_TextLabel::get_width() const {
-    return this->frame.width;
-  }
-
-  const unsigned int X11_TextLabel::get_height() const {
-    return this->frame.height;
-  }
-
-  void X11_TextLabel::move(const int& x, const int& y) {
-    this->frame.move(x,y);
-  }
-
-  void X11_TextLabel::set_position(const int& x, const int& y) {
-    this->frame.set_position(x,y);
+      XSetForeground(display, graphical_context, this->color_scheme[ui::ColorSchemeID::BackgroundColor]);
+      XDrawRectangle(display, window, graphical_context, this->frame.x, this->frame.y, this->frame.width, this->frame.height);
+    }
   }
 
   void X11_TextLabel::update() {
@@ -120,21 +60,17 @@ namespace xlib {
 
     update_frame();
 
-    XSetForeground(display, graphical_context, this->color_scheme.background);
+    XSetForeground(display, graphical_context, this->color_scheme[ui::ColorSchemeID::BackgroundColor]);
     XFillRectangle(display, window, graphical_context, this->frame.x, this->frame.y, this->frame.width, this->frame.height);
 
-    XSetForeground(display, graphical_context, this->color_scheme.text);
+    XSetForeground(display, graphical_context, this->color_scheme[ui::ColorSchemeID::TextColor]);
     XDrawString(display, window, graphical_context, 
         this->frame.x + left_text_margin, 
         this->frame.y + (get_text_graphical_height() + top_text_margin / 2), 
         this->text.c_str(), 
         this->text.length());
 
-    if(focused()) {
-      show_frame();
-    } else {
-      hide_frame();
-    }
+    this->show_frame(focused());
   }
 
   const unsigned int X11_TextLabel::get_text_graphical_width() const {
