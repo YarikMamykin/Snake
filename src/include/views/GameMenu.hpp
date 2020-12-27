c839a1ee553c0324309652bf031940806bca49f7
YarikMamykin <ymamykin@gmail.com>
2020-12-27 23:58:42 +0200

    Update GameMenu with X11_TextLabel

--- src/include/views/GameMenu.hpp
+++ src/include/views/GameMenu.hpp
@@ -3,67 +3,30 @@
 
 #include "View.hpp"
 #include "X11_Window.hpp"
+#include "X11_TextLabel.hpp"
 #include <list>
 #include <functional>
 #include "MouseMotionHandler.hpp"
 #include "MouseButtonPressHandler.hpp"
 #include "KeyPressHandler.hpp"
-#include "Rectangle.hpp"
+#include "AlignHandler.hpp"
 
 namespace views {
 
   class GameMenu final : public View, 
-                         public events::MouseMotionHandler, 
-                         public events::MouseButtonPressHandler, 
                          public events::KeyPressHandler {
     private:
-      struct Item {
-        bool active;
-        std::string name;
-        geometry::Rectangle frame;
-        xlib::X11_Window* x_window;
+      struct Item : public xlib::X11_TextLabel,
+                    public events::KeyPressHandler {
 
-        typedef std::function<void(const int& x, const int& y, const unsigned int& button)> MOUSE_BUTTON_PRESS_HANDLER_TYPE;
-        typedef std::function<void(const int& x, const int& y)> MOUSE_MOTION_HANDLER_TYPE;
         typedef std::function<void(const KeySym&& key_sym)> KEY_PRESS_HANDLER_TYPE;
-        typedef std::vector<std::string> ITEMS_NAMES_VECTOR_TYPE;
-        typedef std::tuple<std::string, 
-                MOUSE_MOTION_HANDLER_TYPE, 
-                MOUSE_BUTTON_PRESS_HANDLER_TYPE, 
-                KEY_PRESS_HANDLER_TYPE> ITEMS_EVENT_HANDLERS_TYPE;
-        typedef std::vector<ITEMS_EVENT_HANDLERS_TYPE> ITEMS_EVENT_HANDLERS_VECTOR_TYPE;
-
-        MOUSE_BUTTON_PRESS_HANDLER_TYPE mouse_button_press_handler;
-        MOUSE_MOTION_HANDLER_TYPE mouse_motion_handler;
-        KEY_PRESS_HANDLER_TYPE key_press_handler;
-
-        static const unsigned int left_text_margin;
-        static const unsigned int top_text_margin;
-        static const unsigned int frame_weight;
-
         Item(xlib::X11_Window* x_window, 
-             bool active, 
              const std::string& name, 
-             const geometry::Rectangle& frame,
-             MOUSE_MOTION_HANDLER_TYPE mouse_motion_handler,
-             MOUSE_BUTTON_PRESS_HANDLER_TYPE mouse_button_press_handler,
              KEY_PRESS_HANDLER_TYPE key_press_handler);
         ~Item();
-        void show_text() const;
-        void show_frame() const;
-        void hide_frame() const;
-        void show_focus();
-        void hide_focus();
-        void show() const;
 
-        int get_width() const;
-        int get_height() const;
-
-        bool focused(const int& x, const int& y) const;
-
-        void handle_mouse_motion(const int& x, const int& y);
-        void handle_button_press(const int& x, const int& y, const unsigned int& button);
         void handle_key_press(const KeySym&& key_sym);
+         KEY_PRESS_HANDLER_TYPE key_press_handler;
       };
 
     public:
@@ -73,17 +36,19 @@ namespace views {
     public:
       void activate() override;
       void deactivate() override;
-      void handle_mouse_motion(const int& x, const int& y) override;
-      void handle_button_press(const int& x, const int& y, const unsigned int& button) override;
       void handle_key_press(const KeySym&& key_sym) override;
       const int get_event_handling_mask() const override;
+      void move_to_next_item();
+      void move_to_prev_item();
 
     private:
       void update();
 
     private:
-      xlib::X11_Window* x_window;
+      xlib::X11_Window* parent_window;
       std::list<Item> items;
+      std::list<Item>::iterator current_item;
+      ui::AlignHandler<Item, ui::LayoutType::VERTICAL> items_layout;
   };
 }
 
--- src/views/GameMenu.cpp
+++ src/views/GameMenu.cpp
@@ -1,10 +1,6 @@
 #include "GameMenu.hpp"
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <tuple>
-#include "EventHandler.hpp"
 #include "Helper.hpp"
+#include "WindowAnchorHandler.hpp"
 
 namespace {
   const std::string NewGameItemName = "New Game"; 
@@ -12,313 +8,110 @@ namespace {
   const std::string SettingsItemName = "Settings"; 
   const std::string ExitItemName = "Exit";
 
-  auto empty_mouse_motion_handler = [](const int& x, const int& y) {};
-  auto empty_mouse_button_press_handler = [](const int& x, const int& y, const unsigned int& button) {};
   auto empty_key_press_handler = [](const KeySym&& key_sym) {};
+
+  abstractions::ui::COLOR_SCHEME_TYPE text_labels_color_scheme = {
+    { ui::ColorSchemeID::BackgroundColor, 0UL },
+    { ui::ColorSchemeID::TextColor, (255UL << 8) },
+    { ui::ColorSchemeID::FrameColor, (255UL << 16) }
+  };
 }
 
 namespace views {
 
-  const unsigned int GameMenu::Item::left_text_margin = 10;
-  const unsigned int GameMenu::Item::top_text_margin = 10;
-  const unsigned int GameMenu::Item::frame_weight = 3;
-
   GameMenu::Item::Item(xlib::X11_Window* x_window, 
-      bool active, 
       const std::string& name, 
-      const geometry::Rectangle& frame,
-      MOUSE_MOTION_HANDLER_TYPE mouse_motion_handler,
-      MOUSE_BUTTON_PRESS_HANDLER_TYPE mouse_button_press_handler,
       KEY_PRESS_HANDLER_TYPE key_press_handler)
-  :x_window(x_window)
-  ,mouse_button_press_handler(mouse_button_press_handler) 
-  ,mouse_motion_handler(mouse_motion_handler) 
-  ,key_press_handler(key_press_handler) {
-      this->active = active;
-      this->name = name;
-      this->frame = frame;
-      this->frame.height = x_window->font_info->ascent + x_window->font_info->descent + top_text_margin * 2;
-  }
+  : xlib::X11_TextLabel(name, {}, text_labels_color_scheme, x_window)
+  , key_press_handler(key_press_handler) { }
 
   GameMenu::Item::~Item() {
   }
 
-  void GameMenu::Item::show_text() const {
-    const auto& window_color_scheme = x_window->get_color_scheme();
-
-    XSetForeground(x_window->x_display.display, 
-        x_window->graphical_context, 
-        window_color_scheme.at(ui::ColorSchemeID::FontColor));
-
-    XDrawString(x_window->x_display.display, 
-        x_window->window, 
-        x_window->graphical_context, 
-        this->frame.x + left_text_margin, 
-        this->frame.y + (x_window->font_info->ascent + x_window->font_info->descent + top_text_margin / 2), 
-        this->name.c_str(), 
-        this->name.length());
-  }
-
-  void GameMenu::Item::show_frame() const {
-    XSetLineAttributes(x_window->x_display.display, x_window->graphical_context,frame_weight,0,0,0);
-    XSetForeground(x_window->x_display.display, x_window->graphical_context, ~0L);
-    XDrawRectangle(x_window->x_display.display,
-        x_window->window,
-        x_window->graphical_context,
-        this->frame.x,
-        this->frame.y,
-        this->frame.width,
-        this->frame.height);
-  }
-
-  void GameMenu::Item::hide_frame() const {
-    XSetLineAttributes(x_window->x_display.display, x_window->graphical_context,frame_weight,0,0,0);
-    XSetForeground(x_window->x_display.display, x_window->graphical_context, 0L);
-    XDrawRectangle(x_window->x_display.display,
-        x_window->window,
-        x_window->graphical_context,
-        this->frame.x,
-        this->frame.y,
-        this->frame.width,
-        this->frame.height);
-  }
-
-  void GameMenu::Item::show() const {
-    this->show_text();
-    if(!active) {
-      this->hide_frame();
-      return;
-    } 
-    this->show_frame();
-  }
-
-  void GameMenu::Item::show_focus() {
-    this->show_text();
-    this->show_frame();
-    this->active = true;
-  }
-
-  void GameMenu::Item::hide_focus() {
-    this->show_text();
-    this->hide_frame();
-    this->active = false;
-  }
-
-  bool GameMenu::Item::focused(const int& x, const int& y) const {
-    return (x >= this->frame.x && (x <= this->frame.x + this->frame.width)) &&
-           (y >= this->frame.y && (y <= this->frame.y + this->frame.height));
-  }
-  
-  int GameMenu::Item::get_width() const {
-    return (XTextWidth(x_window->font_info, 
-          this->name.c_str(), 
-          this->name.length()) + left_text_margin * 2);
-  }
-
-  int GameMenu::Item::get_height() const {
-    return (x_window->font_info->ascent + 
-        x_window->font_info->descent + 
-        top_text_margin * 2);
-  }
-
-  void GameMenu::Item::handle_mouse_motion(const int& x, const int& y) {
-    if(this->focused(x,y)) {
-      this->show_focus();
-      this->mouse_motion_handler(x,y);
-    }
-  }
-
-  void GameMenu::Item::handle_button_press(const int& x, const int& y, const unsigned int& button) {
-    if(this->active) {
-      this->show_focus();
-      if(button == Button1) {
-        this->mouse_button_press_handler(x,y,button);
-      }
-    }
-  }
-
   void GameMenu::Item::handle_key_press(const KeySym&& key_sym) {
-    if(!this->active) {
-      return;
+    if(this->focused()) {
+      this->key_press_handler(std::move(key_sym));
     }
-
-    this->key_press_handler(std::move(key_sym));
   }
 
-  GameMenu::GameMenu(xlib::X11_Window* x_window) :
-    x_window(x_window) {
-      Item::ITEMS_EVENT_HANDLERS_VECTOR_TYPE items_event_handlers = {
-        {
-          NewGameItemName,
-          empty_mouse_motion_handler,
-          [](const int& x, const int& y, const unsigned int& button) {
-          },
-          [x_window](const KeySym&& key_sym) {
-            switch(key_sym) {
-              case XK_Return: helpers::Helper::SendChangeViewEvent(x_window, views::ViewID::ACTION); break;
-            }
-          }
-        },
-        {
-          ScoreItemName,
-          empty_mouse_motion_handler,
-          empty_mouse_button_press_handler,
-          empty_key_press_handler
-        },
-        {
-          SettingsItemName,
-          empty_mouse_motion_handler,
-          empty_mouse_button_press_handler,
-          [x_window](const KeySym&& key_sym) {
-            switch(key_sym) {
-              case XK_Return: helpers::Helper::SendChangeViewEvent(x_window, views::ViewID::SETTINGS); break;
-            }
-          }
-        },
-        {
-          ExitItemName,
-          empty_mouse_motion_handler,
-          empty_mouse_button_press_handler,
-          [x_window](const KeySym&& key_sym) {
-            switch(key_sym) {
-              case XK_Return: helpers::Helper::SendExitApplicationEvent(x_window); break;
-            }
-          }
-        },
-      };
+  GameMenu::GameMenu(xlib::X11_Window* x_window) 
+  : parent_window(x_window) {
+    items.emplace_back(Item(x_window, NewGameItemName, [x_window](const KeySym&& key_sym) {
+          switch(key_sym) {
+            case XK_Return: helpers::Helper::SendChangeViewEvent(x_window, views::ViewID::ACTION); break;
+          }}));
+    items.emplace_back(Item(x_window, ScoreItemName, empty_key_press_handler));
+    items.emplace_back(Item(x_window, SettingsItemName, [x_window](const KeySym&& key_sym) {
+          switch(key_sym) {
+            case XK_Return: helpers::Helper::SendChangeViewEvent(x_window, views::ViewID::SETTINGS); break;
+          }}));
+    items.emplace_back(Item(x_window, ExitItemName, [x_window](const KeySym&& key_sym) {
+          switch(key_sym) {
+            case XK_Return: helpers::Helper::SendExitApplicationEvent(x_window); break;
+          }}));
 
-      const auto&& item_with_longest_name = std::max_element(items_event_handlers.begin(), items_event_handlers.end(), 
-      [](const Item::ITEMS_EVENT_HANDLERS_TYPE& item1, const Item::ITEMS_EVENT_HANDLERS_TYPE& item2) {
-        return std::get<0>(item1).size() < std::get<0>(item2).size();
-      });
-      const auto& item_longest_name = std::get<0>(*item_with_longest_name);
+    current_item = items.begin();
+    current_item->set_focused(true);
 
-      const auto max_item_width = XTextWidth(x_window->font_info, item_longest_name.c_str(), item_longest_name.size()) + GameMenu::Item::left_text_margin * 2;
-      const auto&& x_pos = x_window->get_width() / 2 - max_item_width / 2;
-
-      auto summary_height = 0;
-      const bool active = false;
-      for(auto& item : items_event_handlers) {
-        this->items.emplace_back(
-            Item(x_window, 
-              active, 
-              std::get<0>(item),
-              { 
-                .x = x_pos, 
-                .y = x_window->get_height() / 3 + summary_height + GameMenu::Item::frame_weight,
-                .width = max_item_width 
-              },
-              std::get<1>(item),
-              std::get<2>(item),
-              std::get<3>(item)
-        ));
-        summary_height += items.back().get_height();
-      }
-
-      std::for_each(items.begin(), items.end(), [](const Item& item) { item.show(); });
-      items.begin()->show_focus();
+    for(auto& item : items) {
+      items_layout.add(&item);
     }
+  }
 
   GameMenu::~GameMenu() {
   }
 
   void GameMenu::activate() {
-    this->update();
-    for(auto& item : items) {
-      item.hide_frame();
-      item.show_text();
-    }
-
-    auto item_to_show = std::find_if(items.begin(), items.end(), [](const Item& item) { return item.active; });
-    if(item_to_show != items.end()) {
-      item_to_show->show();
-    }
+    update();
   }
 
   void GameMenu::update() {
-    const auto&& x_pos = x_window->get_width() / 2 - items.begin()->frame.width / 2;
-    auto summary_height = 0;
-
-    for(auto& item : items) {
-      item.frame.x = x_pos;
-      item.frame.y = x_window->get_height() / 3 + summary_height + GameMenu::Item::frame_weight;
-      summary_height += item.get_height();
-    }
+    current_item->set_focused(true);
+    ui::WindowAnchorHandler<decltype(items_layout)> anchor_handler(&items_layout, parent_window);
+    items_layout.update();
   }
 
-  void GameMenu::deactivate() {
-  }
-
-  void GameMenu::handle_mouse_motion(const int& x, const int& y) {
-    this->update();
-
-    for(auto& item : items) {
-      item.hide_focus();
-    }
-
-    for(auto& item : items) {
-      item.handle_mouse_motion(x,y);
-    }
-  }
-
-  void GameMenu::handle_button_press(const int& x, const int& y, const unsigned int& button) {
-    this->update();
-
-    for(auto& item : items) {
-      item.hide_focus();
-    }
-
-    for(auto& item : items) {
-      item.handle_button_press(x,y,button);
-    }
-  }
+  void GameMenu::deactivate() { }
 
   void GameMenu::handle_key_press(const KeySym&& key_sym) {
-    this->update();
     switch(key_sym) {
-      case XK_Down:
-        {
-          const auto& active_item = std::find_if(items.begin(), items.end(), [](const Item& item) { return item.active; });
-          for(auto& item : items) {
-            item.hide_focus();
-          }
-
-          if(active_item == --items.end()) {
-            items.begin()->show_focus();
-            break;
-          }
-
-          std::next(active_item)->show_focus();
-          break;
-        }
-      case XK_Up:
-        {
-          auto active_item = std::find_if(items.begin(), items.end(), [](const Item& item) { return item.active; });
-          for(auto& item : items) {
-            item.hide_focus();
-          }
-
-          if(active_item == items.begin()) {
-            items.back().show_focus();
-            return;
-          }
-
-          std::prev(active_item)->show_focus();
-          break;
-        }   
-      case XK_Return:
-        {
-          for(auto& item : items) {
-            item.handle_key_press(std::move(key_sym));
-          }
-          break;
-        }
+      case XK_Down: move_to_next_item(); break;
+      case XK_Up: move_to_prev_item(); break;
+      case XK_Return: current_item->handle_key_press(std::move(key_sym)); break; 
     }
+
+    update();
+  }
+
+  void GameMenu::move_to_next_item() {
+    for(auto& item : items) {
+      item.set_focused(false);
+    }
+
+    if(current_item == --items.end()) {
+      current_item = items.begin();
+      return;
+    }
+
+    current_item = std::next(current_item);
+    return;
+  }
+
+  void GameMenu::move_to_prev_item() {
+    for(auto& item : items) {
+      item.set_focused(false);
+    }
+
+    if(current_item == items.begin()) {
+      current_item = --items.end();
+      return;
+    }
+
+    current_item = std::prev(current_item);
+    return;
   }
 
   const int GameMenu::get_event_handling_mask() const {
-    return events::KeyPressHandler::get_event_handling_mask() | 
-           events::MouseMotionHandler::get_event_handling_mask() | 
-           events::MouseButtonPressHandler::get_event_handling_mask();
+    return events::KeyPressHandler::get_event_handling_mask();
   }
 }
