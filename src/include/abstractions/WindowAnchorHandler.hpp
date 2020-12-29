6a156a8d70d0ed8319d38a480b96edf2949d841b
YarikMamykin <ymamykin@gmail.com>
2020-12-29 07:32:40 +0200

    Refactor WindowAnchorHandler

--- src/include/abstractions/WindowAnchorHandler.hpp
+++ src/include/abstractions/WindowAnchorHandler.hpp
@@ -2,7 +2,7 @@
 #define SRC_INCLUDE_ABSTRACTIONS_WINDOWANCHORHANDLER_HPP
 
 #include "Constants.hpp"
-#include "Window.hpp"
+#include "X11_Window.hpp"
 #include "Rectangle.hpp"
 
 namespace ui {
@@ -14,15 +14,20 @@ namespace ui {
   template <class Object, AnchorType anchor_type = AnchorType::CENTER>
     class WindowAnchorHandler {
       public:
-        WindowAnchorHandler(Object* object, abstractions::ui::AWindow* parent_window) {
-          auto&& window_center = get_window_center(parent_window);
-          auto&& object_width = object->get_width();
-          auto&& object_height = object->get_height();
-          object->set_position(window_center.x - object_width/2, window_center.y - object_height/2);
+        WindowAnchorHandler(Object* object, xlib::X11_Window* parent_window) {
+          switch(anchor_type) {
+            case AnchorType::CENTER:           
+              {
+                auto&& window_center = get_window_center(parent_window);
+                object->set_center(window_center.x, window_center.y);
+                break;
+              }
+          }
         }
 
       private:
-        geometry::Point get_window_center(abstractions::ui::AWindow* parent_window) {
+        geometry::Point get_window_center(xlib::X11_Window* parent_window) {
+          parent_window->update_window_frame();
           return { .x = parent_window->get_width()/2, .y = parent_window->get_height()/2 };
         }
     };
