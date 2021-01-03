e3d4d9293e045e8e53f3b4a1ef3642dc5de8327d
YarikMamykin <ymamykin@gmail.com>
2021-01-03 14:44:44 +0200

    Refactor GameSettings with X11_Menu

--- src/include/views/GameSettings.hpp
+++ src/include/views/GameSettings.hpp
@@ -5,10 +5,11 @@
 #include "X11_Window.hpp"
 #include "KeyPressHandler.hpp"
 #include "X11_TextLabel.hpp"
-#include "AlignHandler.hpp"
+#include "X11_Menu.hpp"
 
 #include <string>
 #include <list>
+#include <memory>
 
 namespace views {
 
@@ -16,20 +17,16 @@ namespace views {
                              public events::KeyPressHandler {
 
     private:
-      typedef ui::AlignHandler<xlib::X11_TextLabel, ui::LayoutType::HORIZONTAL> LAYOUT_TYPE;
-      typedef ui::AlignHandler<LAYOUT_TYPE, ui::LayoutType::VERTICAL> V_LAYOUT_TYPE;
+      struct Setting : public xlib::X11_Menu {
+        Setting(std::unique_ptr<abstractions::ui::TextLabel> key_presenter, 
+                std::unique_ptr<abstractions::ui::Object> value_presenter,
+                xlib::X11_Window* x_window);
 
-      struct Setting {
-        std::string key;
-        unsigned int value;
-        bool active;
+        void increase();
+        void decrease();
 
-        xlib::X11_TextLabel key_label;
-        xlib::X11_TextLabel value_label;
-
-        Setting(const std::string& key, unsigned int value, xlib::X11_Window* parent_window);
-        void set_active(bool active);
-        void update_value(const unsigned int& new_value);
+        std::function<void()> increase_binder;
+        std::function<void()> decrease_binder;
       };
 
     public:
@@ -43,17 +40,30 @@ namespace views {
 
     private:
       void update();
-      void move_to_next_item();
-      void move_to_prev_item();
-      void increase_setting_value();
-      void decrease_setting_value();
+      template<typename ValuePresenterType, typename ValueType, typename ValuePresentingObjectType> std::unique_ptr<xlib::X11_Menu>
+        construct_menu_item(const std::string& name, const ValueType& value, 
+                            const abstractions::ui::COLOR_SCHEME_TYPE& key_color_scheme,
+                            const abstractions::ui::COLOR_SCHEME_TYPE& value_color_scheme,
+                            std::unique_ptr<ValuePresentingObjectType> value_presenting_ui_object,
+                            xlib::X11_Window* x_window) {
+          std::unique_ptr<xlib::X11_TextLabel> key_presenter(new xlib::X11_TextLabel(name, {}, key_color_scheme, x_window));
+          std::unique_ptr<ValuePresenterType> value_presenter(new ValuePresenterType(value, std::move(value_presenting_ui_object)));
+
+          auto increase_binder = value_presenter->bind_increase_value_trigger();
+          auto decrease_binder = value_presenter->bind_decrease_value_trigger();
+
+          auto setting = new Setting(std::move(key_presenter), std::move(value_presenter), x_window);
+
+          setting->increase_binder = increase_binder;
+          setting->decrease_binder = decrease_binder;
+
+          std::unique_ptr<xlib::X11_Menu> menu_item(setting);
+          return menu_item;
+        }
 
     private:
       xlib::X11_Window* x_window;
-      std::list<Setting> settings_items;
-      std::list<Setting>::iterator current_active_item;
-      std::list<LAYOUT_TYPE> labels_layout;
-      V_LAYOUT_TYPE horizontal_layout;
+      xlib::X11_Menu menu;
   };
 }
 
--- src/views/GameSettings.cpp
+++ src/views/GameSettings.cpp
@@ -3,6 +3,8 @@
 #include "Settings.hpp"
 #include <iostream>
 #include "WindowAnchorHandler.hpp"
+#include "UlongValuePresenter.hpp"
+#include "ColorValuePresenter.hpp"
 
 namespace {
   abstractions::ui::COLOR_SCHEME_TYPE key_color_scheme = {
@@ -16,46 +18,60 @@ namespace {
     { ui::ColorSchemeID::FrameColor, ~0UL },
     { ui::ColorSchemeID::TextColor, (127UL << 16)|(255UL << 8)|212UL }
   };
+
+  abstractions::ui::COLOR_SCHEME_TYPE menu_color_scheme = {
+    { ui::ColorSchemeID::BackgroundColor, 0UL },
+    { ui::ColorSchemeID::FrameColor, ~0UL }
+  };
 }
 
 namespace views {
-  GameSettings::Setting::Setting(const std::string& key, unsigned int value, xlib::X11_Window* parent_window) 
-  : key(key)
-  , value(value)
-  , key_label(key, {}, key_color_scheme, parent_window)
-  , value_label(std::to_string(value), {}, value_color_scheme, parent_window) { }
-
-  void GameSettings::Setting::set_active(bool active) {
-    this->active = active;
-    key_label.set_focused(this->active);
-    value_label.set_focused(this->active);  
+  GameSettings::Setting::Setting(std::unique_ptr<abstractions::ui::TextLabel> key_presenter, 
+                                 std::unique_ptr<abstractions::ui::Object> value_presenter,
+                                 xlib::X11_Window* x_window) 
+  : xlib::X11_Menu(::ui::LayoutType::HORIZONTAL, {}, menu_color_scheme, x_window, 30U) {
+    add_item(std::move(key_presenter));
+    add_item(std::move(value_presenter));
+    margin = 0U;
   }
 
-  void GameSettings::Setting::update_value(const unsigned int& new_value) {
-    value = new_value;
-    value_label.set_text(std::to_string(new_value));
+  void GameSettings::Setting::increase() {
+    increase_binder();
+  }
+
+  void GameSettings::Setting::decrease() {
+    decrease_binder();
   }
 }
 
 namespace views {
 
   GameSettings::GameSettings(xlib::X11_Window* x_window) 
-  : x_window(x_window) 
-  , horizontal_layout({}) {
-    settings_items.emplace_back(Setting("Snake speed: ", settings::Settings::settings().snake_speed, x_window));
-    settings_items.emplace_back(Setting("Snake color: ", settings::Settings::settings().snake_color, x_window));
+  : x_window(x_window)  
+  , menu(ui::LayoutType::VERTICAL, {}, abstractions::ui::COLOR_SCHEME_TYPE(), x_window, 20U) {
 
-    settings_items.front().set_active(true);
-    current_active_item = settings_items.begin();
+    std::unique_ptr<xlib::X11_TextLabel> text_label(new xlib::X11_TextLabel(std::to_string(settings::Settings::settings().snake_speed), {}, value_color_scheme, x_window));
+    std::unique_ptr<xlib::X11_ColorLabel> color_label(new xlib::X11_ColorLabel(0UL, {.width = 100U, .height = text_label->get_height()}, value_color_scheme, x_window));
 
-    for(auto& item : settings_items) {
-      auto&& layout = LAYOUT_TYPE({});
-      layout.add(&(item.key_label));
-      layout.add(&(item.value_label));
-      labels_layout.push_back(layout);
-      horizontal_layout.add(&labels_layout.back());
-    }
-    ui::WindowAnchorHandler<V_LAYOUT_TYPE> anchor_handler(&horizontal_layout, x_window);
+    menu.add_item(std::move(construct_menu_item<ui::UlongValuePresenter, 
+                                                unsigned long, 
+                                                xlib::X11_TextLabel>(
+            "Snake speed: ", 
+            settings::Settings::settings().snake_speed, 
+            key_color_scheme,
+            value_color_scheme,
+            std::move(text_label),
+            x_window)));
+
+    menu.add_item(std::move(construct_menu_item<ui::ColorValuePresenter, 
+                                                abstractions::ui::ColorLabel::Color, 
+                                                xlib::X11_ColorLabel>(
+            "Snake color: ", 
+            settings::Settings::settings().snake_color, 
+            key_color_scheme,
+            value_color_scheme,
+            std::move(color_label),
+            x_window)));
   }
 
   GameSettings::~GameSettings() {
@@ -71,56 +87,19 @@ namespace views {
   void GameSettings::handle_key_press(const KeySym&& key_sym) {
     switch(key_sym) {
       case XK_Escape: helpers::Helper::SendChangeViewEvent(x_window, views::ViewID::MENU); break;
-      case XK_Down: move_to_next_item(); break;
-      case XK_Up: move_to_prev_item(); break;
-      case XK_KP_Add: increase_setting_value(); break;
-      case XK_KP_Subtract: decrease_setting_value(); break;
+      case XK_Down: menu.move_to_next_item(); break;
+      case XK_Up: menu.move_to_prev_item(); break;
+      case XK_KP_Add: static_cast<GameSettings::Setting*>(menu.get_current_item()->get())->increase(); break;
+      case XK_KP_Subtract: static_cast<GameSettings::Setting*>(menu.get_current_item()->get())->decrease(); break;
     }
 
     update();
   }
 
   void GameSettings::update() {
-    horizontal_layout.update();
+    ui::WindowAnchorHandler<decltype(menu)> anchor_handler(&menu, x_window);
+    menu.get_current_item()->get()->set_focused(true);
+    menu.show(true);
   }
 
-  void GameSettings::move_to_next_item() {
-    for(auto& item : settings_items) {
-      item.set_active(false);
-    }
-
-    if(current_active_item == --settings_items.end()) {
-      settings_items.begin()->set_active(true);
-      current_active_item = settings_items.begin();
-      return;
-    }
-
-    current_active_item = std::next(current_active_item);
-    current_active_item->set_active(true);
-    return;
-  }
-
-  void GameSettings::move_to_prev_item() {
-    for(auto& item : settings_items) {
-      item.set_active(false);
-    }
-
-    if(current_active_item == settings_items.begin()) {
-      settings_items.back().set_active(true);
-      current_active_item = --settings_items.end();
-      return;
-    }
-
-    current_active_item = std::prev(current_active_item);
-    current_active_item->set_active(true);
-    return;
-  }
-
-  void GameSettings::increase_setting_value() {
-    current_active_item->update_value(current_active_item->value + 1U);
-  }
-
-  void GameSettings::decrease_setting_value() {
-    current_active_item->update_value(current_active_item->value - 1U);
-  }
 }
