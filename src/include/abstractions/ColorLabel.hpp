94167f17314358e9e0d46a8237343b243951d25f
YarikMamykin <ymamykin@gmail.com>
2021-01-04 00:05:46 +0200

    Fix explicit construction from hex values

--- src/include/abstractions/ColorLabel.hpp
+++ src/include/abstractions/ColorLabel.hpp
@@ -15,15 +15,16 @@ namespace abstractions {
             Color() = default;
             Color(const unsigned long& value) : red(value >> 16), green(value >> 8), blue(value) {}
             Color(const std::string& value) {
-              const bool value_contains_hash_char = (value.find_first_of('#') != 0u);
-              const bool value_length_is_enough = (value.size() != 7u);
+              const bool value_contains_hash_char = (value.find_first_of('#') == 0u);
+              const bool value_length_is_enough = (value.size() == 7u);
               if(!value_contains_hash_char || !value_length_is_enough) {
                 return;
               }
 
-              std::stringstream(value.substr(1u, 2)) >> std::hex >> red;
-              std::stringstream(value.substr(3u, 2)) >> std::hex >> green;
-              std::stringstream(value.substr(5u, 2)) >> std::hex >> blue;
+              unsigned short buffer;
+              std::stringstream(value.substr(1u, 2)) >> std::hex >> buffer; red = buffer;
+              std::stringstream(value.substr(3u, 2)) >> std::hex >> buffer; green = buffer;
+              std::stringstream(value.substr(5u, 2)) >> std::hex >> buffer; blue = buffer;
             }
 
             const unsigned long to_long() const { 
