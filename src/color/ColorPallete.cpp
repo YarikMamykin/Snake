1d86e8f7b91ccdc603d2e3b79dc91eebe68d8909
YarikMamykin <ymamykin@gmail.com>
2021-01-10 18:38:56 +0200

    Refactor ColorPallete

--- src/color/ColorPallete.cpp
+++ src/color/ColorPallete.cpp
@@ -1,30 +1,35 @@
 #include "ColorPallete.hpp"
 #include <algorithm>
-#include <iostream>
+
+namespace {
+  const std::list<color::Color> default_colors = { 
+    color::Color("#ff0000"), 
+    color::Color("#00ff00"), 
+    color::Color("#0000ff") 
+  };
+}
 
 namespace color {
   ColorPallete::ColorPallete() 
-    : colors({ Color("#ff0000"), Color("#00ff00"), Color("#0000ff") }) 
+    : colors(default_colors) 
     , current_color(colors.begin()) { }
 
   ColorPallete::ColorPallete(const std::string& hex_color)
-    : ColorPallete() {
-      set_current_color(Color(hex_color));
-    }
+    : ColorPallete(Color(hex_color)) { }
 
   ColorPallete::ColorPallete(const Color& color)
     : ColorPallete() {
       set_current_color(color);
     }
 
-  ColorPallete::ColorPallete(const ColorPallete& color_pallete) 
-    : colors(color_pallete.colors) 
+  ColorPallete::ColorPallete(const ColorPallete& color_pallete)
+    : colors(color_pallete.colors)
       , current_color(colors.begin()) {
         set_current_color(color_pallete.get_current_color());
       }
 
   ColorPallete& ColorPallete::operator ++() {
-    if(current_color == std::prev(colors.end()) || current_color == colors.end()) {
+    if(current_color == std::prev(colors.end())) {
       current_color = colors.begin();
     } else {
       current_color = std::next(current_color);
@@ -48,14 +53,8 @@ namespace color {
   void ColorPallete::set_current_color(const Color& color) {
     current_color = std::find(colors.begin(), colors.end(), color);
     if(current_color == colors.end()) {
-      std::cout << "NEW COLOR ADDED! " << color.to_string() << std::endl;
       colors.push_front(color);
       current_color = colors.begin();
-    }
-
-    std::cout << "COLOR_PALLETE SIZE: " << colors.size() << std::endl;
-    for(auto& color : colors) {
-      std::cout << "COLOR: " << color.to_string() << std::endl;
     }
   }
 }
