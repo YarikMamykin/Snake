b535bf679d3d83b64704cb490a623eeb0eeb7473
YarikM <ymamykin@gmail.com>
2021-01-28 04:54:45 +0200

    Refactor Settings with ObservableValue update

--- src/configuration/Settings.cpp
+++ src/configuration/Settings.cpp
@@ -4,24 +4,24 @@
 
 namespace configuration {
   std::map<ConfigID, std::shared_ptr<abstractions::ObservableValueContainerWrapper>> Settings::settings_map = {
-    { ConfigID::WINDOW_FRAME, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<geometry::Rectangle>({ .x = 0, .y = 0, .width = 200U, .height = 200U })))) },
-    { ConfigID::WINDOW_COLOR_SCHEME, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<color::COLOR_SCHEME_TYPE>({ 
+    { ConfigID::WINDOW_FRAME, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<geometry::Rectangle>(0, 0, 200U, 200U)))) },
+    { ConfigID::WINDOW_COLOR_SCHEME, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<color::COLOR_SCHEME_TYPE>(color::COLOR_SCHEME_TYPE({ 
               { color::ColorSchemeID::FrameColor, color::Color("#ff0000") }, 
               { color::ColorSchemeID::BackgroundColor, color::Color("#000000") }, 
               { color::ColorSchemeID::FontColor, color::Color("#00ff00") } 
-              })))) 
+              })))))
     },
     { ConfigID::WINDOW_BORDER_WIDTH, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<unsigned int>(1u)))) },
     { ConfigID::WINDOW_NAME, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<std::string>("Snake")))) },
     { ConfigID::WINDOW_FONT_NAME, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<std::string>("*-courier-*-24-*")))) },
-    { ConfigID::SNAKE_SPEED, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<SNAKE_SPEED_TYPE>(SNAKE_SPEED_TYPE(5ul, 1ul, 5ul))))) },
-    { ConfigID::SNAKE_COLOR, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<color::ColorPallete>(std::string("#00ff00"))))) },
+    { ConfigID::SNAKE_SPEED, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<RESTRICTED_ULONG>(5ul, 1ul, 5ul)))) },
+    { ConfigID::SNAKE_COLOR, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<color::ColorPallete>("#00ff00")))) },
     { ConfigID::SNAKE_TIMEOUT, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<std::chrono::milliseconds>(std::chrono::milliseconds(50u))))) },
     { ConfigID::GAME_OVER_TIMEOUT, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<std::chrono::milliseconds>(std::chrono::milliseconds(80u))))) },
-    { ConfigID::SNAKE_HEAD_X, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<unsigned long>(80u)))) },
-    { ConfigID::SNAKE_HEAD_Y, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<unsigned long>(80u)))) },
-    { ConfigID::SNAKE_HEAD_WIDTH, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<unsigned long>(20u)))) },
-    { ConfigID::SNAKE_HEAD_HEIGHT, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<unsigned long>(10u)))) },
+    { ConfigID::SNAKE_HEAD_X, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<RESTRICTED_INT>(80, 10, 2000)))) },
+    { ConfigID::SNAKE_HEAD_Y, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<RESTRICTED_INT>(80, 10, 2000)))) },
+    { ConfigID::SNAKE_HEAD_WIDTH, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<RESTRICTED_UINT>(20ul, 5ul, 50ul)))) },
+    { ConfigID::SNAKE_HEAD_HEIGHT, std::shared_ptr<abstractions::ObservableValueContainerWrapper>(static_cast<abstractions::ObservableValueContainerWrapper*>((new abstractions::ObservableValue<RESTRICTED_UINT>(10ul, 5ul, 50ul)))) },
   };
   
   std::shared_ptr<abstractions::ObservableValueContainerWrapper> Settings::get(ConfigID id) {
