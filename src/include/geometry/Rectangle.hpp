c86c5078795ff907b660881b60b4f872ac06bd51
YarikM <ymamykin@gmail.com>
2021-01-30 03:06:18 +0200

    Add move semantics to Rectangle

--- src/include/geometry/Rectangle.hpp
+++ src/include/geometry/Rectangle.hpp
@@ -84,7 +84,7 @@ namespace geometry {
       return rect_data.str();
     }
 
-    bool has_point(const Point& p) const {
+    bool has_point(Point&& p) const {
       const auto&& top_left_p = top_left();
       const auto&& bottom_right_p = bottom_right();
       return p >= top_left_p && p >= bottom_right_p;
