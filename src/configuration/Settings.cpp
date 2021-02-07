353156a2223465a9faa99a81c3dbe21542f9d706
YarikM <ymamykin@gmail.com>
2021-02-08 00:53:15 +0200

    Change default food size

--- src/configuration/Settings.cpp
+++ src/configuration/Settings.cpp
@@ -23,7 +23,7 @@ namespace configuration {
     { ConfigID::RANDOM_ENGINE_ALGORITHM, (new abstractions::ObservableValue<RANDOM_ENGINE_ALGORITHM_TYPE>())->wrap() },
     { ConfigID::RANDOM_ENGINE_SEED_SEQUENCE, (new abstractions::ObservableValue<RANDOM_ENGINE_SEED_SEQUENCE_TYPE>(std::list<unsigned int>{1u,2u,3u,4u,5u,6u,7u,8u}))->wrap() },
     { ConfigID::FOOD_COLOR, (new abstractions::ObservableValue<color::ColorPallete>("#ffff00"))->wrap() },
-    { ConfigID::FOOD_SIZE, (new abstractions::ObservableValue<RESTRICTED_UINT>(10u, 5u, 20u))->wrap() },
+    { ConfigID::FOOD_SIZE, (new abstractions::ObservableValue<RESTRICTED_UINT>(15u, 5u, 20u))->wrap() },
   };
   
   std::shared_ptr<abstractions::ObservableValueContainerWrapper> Settings::get(ConfigID id) {
