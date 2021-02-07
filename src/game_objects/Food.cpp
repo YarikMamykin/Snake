bd00a9462ff1e41a3e50343d55ac9acb7e68acfa
YarikM <ymamykin@gmail.com>
2021-02-08 00:57:00 +0200

    Refactor Food

--- src/game_objects/Food.cpp
+++ src/game_objects/Food.cpp
@@ -3,10 +3,10 @@
 
 namespace game_objects {
 
-  Food::Food(const color::Color&& background_color, const color::Color&& color, geometry::Rectangle&& frame) 
+  Food::Food(color::Color&& background_color, color::Color&& color, geometry::Rectangle&& frame) 
   : background_color(background_color) 
   , color(color) 
-  , frame(std::forward<decltype(frame)>(frame)) { }
+  , frame(std::move(frame)) { }
 
   Food::~Food() {
     xlib::XlibWrapper::self()->fill_circle(std::forward<decltype(frame)>(frame), 
--- src/include/game_objects/Food.hpp
+++ src/include/game_objects/Food.hpp
@@ -1,16 +1,16 @@
 #ifndef SRC_INCLUDE_GAME_OBJECTS_FOOD_HPP
 #define SRC_INCLUDE_GAME_OBJECTS_FOOD_HPP
 
-#include "X11_Window.hpp"
+#include "Rectangle.hpp"
 #include "Color.hpp"
 
 namespace game_objects {
-  class Food final {
+  struct Food final {
     color::Color background_color, color; 
     geometry::Rectangle frame;
 
     public:
-    explicit Food(const color::Color&& background_color, const color::Color&& color, geometry::Rectangle&& frame);
+    Food(color::Color&& background_color, color::Color&& color, geometry::Rectangle&& frame);
     ~Food();
 
     void show();
