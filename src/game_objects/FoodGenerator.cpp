8286e4b1605b70fdba5853821a04f42c36f937a6
YarikM <ymamykin@gmail.com>
2021-02-23 00:49:36 +0200

    Refactor FoodGenerator

--- src/game_objects/FoodGenerator.cpp
+++ src/game_objects/FoodGenerator.cpp
@@ -2,6 +2,11 @@
 #include "Point.hpp"
 #include "ColorPallete.hpp"
 
+namespace {
+    constexpr int weird_number = 123/4*5-70+3;
+    constexpr int another_weird_number = 390*2/5+30-2*8/7;
+}
+
 namespace game_objects {
   RandomEngine::RandomEngine(std::list<unsigned int>&& seed_sequence) {
     std::seed_seq sequence(seed_sequence.begin(), seed_sequence.end());
@@ -14,8 +19,6 @@ namespace game_objects {
      * in some close field around the snake head.
      */
     auto&& generated_number = algorithm();
-    constexpr int weird_number = 123/4*5-70+3;
-    constexpr int another_weird_number = 390*2/5+30-2*8/7;
     int&& x = generated_number ^ weird_number;
     int&& y = generated_number ^ another_weird_number;
     const bool x_in_range = x >= region.x && x <= (region.x + region.width);
@@ -33,18 +36,16 @@ namespace game_objects {
   Food* FoodGenerator::generate(xlib::X11_Window* x_window) {
     auto&& window_width = x_window->get_width();
     auto&& window_height = x_window->get_height();
-    auto background_color = x_window->get_color_scheme().at(color::ColorSchemeID::BackgroundColor);
+    auto& background_color = x_window->get_color_scheme().at(color::ColorSchemeID::BackgroundColor);
     auto&& food_color = configuration::Settings::get_concrete<color::ColorPallete>(configuration::ConfigID::FOOD_COLOR).get_current_color();
-    auto&& food_size = configuration::Settings::get_concrete<configuration::RESTRICTED_UINT>(configuration::ConfigID::FOOD_SIZE).get_restricted_value();
+    auto&& food_size = configuration::Settings::get_concrete<configuration::RESTRICTED_UINT>(configuration::ConfigID::SNAKE_SIZE).get_restricted_value() * 15u + 5u;
     auto&& region = geometry::Rectangle{200,200,x_window->get_width() - 300u,x_window->get_height() - 300u};
+
     geometry::Point&& generated_point{};
     while((generated_point = points_generator.generate_point(std::move(region))) == prev_point);
     prev_point = generated_point;
-    return new Food(std::forward<decltype(background_color)>(background_color), 
-                    std::forward<decltype(food_color)>(food_color), 
-                    { generated_point.x, 
-                    generated_point.y, 
-                    food_size, 
-                    food_size });
+    auto&& food_frame = geometry::Rectangle{ generated_point.x, generated_point.y, food_size, food_size };
+
+    return new Food(color::Color(background_color), color::Color(food_color), std::move(food_frame));
   }
 }
