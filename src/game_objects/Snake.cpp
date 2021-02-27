73a5419a2632ea2eef0b69cbc997db8cfde10d2d
YarikM <ymamykin@gmail.com>
2021-02-27 14:32:27 +0200

    Place snake head at window center on startup

--- src/game_objects/Snake.cpp
+++ src/game_objects/Snake.cpp
@@ -113,6 +113,7 @@ namespace game_objects {
     constexpr unsigned int spacing = 10u;
 
     parts.emplace_back(SnakeHead(color, std::move(head_shape), SnakeDirection(current_direction), RotationDirection::NONE, spacing));
+    parts.back().frame.set_center(x_window->get_width()/2u, x_window->get_height()/2u);
   }
 
   Snake::~Snake() {
