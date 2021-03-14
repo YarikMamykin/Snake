09894ba400c571adbd574d9dd4a08b1b9d597739
YarikM <ymamykin@gmail.com>
2021-03-15 01:44:53 +0200

    Refactor CenterWindowAnchorHandler with Commands

--- src/anchor_handlers/CenterWindowAnchorHandler.cpp
+++ src/anchor_handlers/CenterWindowAnchorHandler.cpp
@@ -1,5 +1,5 @@
 #include "CenterWindowAnchorHandler.hpp"
-#include "XlibWrapper.hpp"
+#include "QueryWindowAttributes.hpp"
 
 namespace ui {
   CenterWindowAnchorHandler::CenterWindowAnchorHandler(abstractions::ui::Object* object) {
@@ -7,7 +7,9 @@ namespace ui {
   }
 
   geometry::Point CenterWindowAnchorHandler::get_window_center() {
-    auto&& win_attr = xlib::XlibWrapper::self()->get_window_attributes();
+    commands::Command::push_xlib_command(new commands::QueryWindowAttributes());
+    std::unique_ptr<commands::Command> win_attr_command_result = commands::Command::get_command_with_result(commands::CommandID::QueryWindowAttributes);
+    const auto& win_attr = dynamic_cast<commands::QueryWindowAttributes*>(win_attr_command_result.get())->get_window_attributes();
     return { .x = win_attr.width/2, .y = win_attr.height/2 };
   }
 }
