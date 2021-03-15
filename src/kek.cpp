ec5db89f5c104982f41c565784296dae536d0967
YarikM <ymamykin@gmail.com>
2021-03-15 02:36:39 +0200

    Make separate threads for xlib and ui

--- src/kek.cpp
+++ src/kek.cpp
@@ -6,33 +6,77 @@
 
 #include "X11_Window.hpp"
 #include "XlibWrapper.hpp"
-#include "EventHandler.hpp"
+#include "EventDispatcher.hpp"
 #include "Settings.hpp"
+#include "Command.hpp"
+#include "Helper.hpp"
+#include "ChangeView.hpp"
 #include <iostream>
 #include <memory>
-
-void LaunchApp() {
-  using namespace xlib;
-  using namespace events;
-  using namespace configuration;
-  XInitThreads();
-
-  xlib::WindowSettings win_sets = {
-    .frame = abstractions::ObservableValueContainerWrapper::to_concrete_value<geometry::Rectangle>(Settings::get(ConfigID::WINDOW_FRAME))->get_value(),
-    .color_scheme = abstractions::ObservableValueContainerWrapper::to_concrete_value<color::COLOR_SCHEME_TYPE>(Settings::get(ConfigID::WINDOW_COLOR_SCHEME))->get_value(),
-    .border_width = abstractions::ObservableValueContainerWrapper::to_concrete_value<unsigned int>(Settings::get(ConfigID::WINDOW_BORDER_WIDTH))->get_value(),
-    .name = abstractions::ObservableValueContainerWrapper::to_concrete_value<std::string>(Settings::get(ConfigID::WINDOW_NAME))->get_value(),
-    .font_name = abstractions::ObservableValueContainerWrapper::to_concrete_value<std::string>(Settings::get(ConfigID::WINDOW_FONT_NAME))->get_value()
-  };
-
-  std::shared_ptr<abstractions::ui::AWindow> x_window(new X11_Window(views::ViewID::MENU, win_sets));
-  EventHandler ehandler(x_window);
-  ehandler.event_handler_loop();
-}
+#include <thread>
+#include <functional>
+#include <atomic>
 
 int main(void)
 {
-  LaunchApp();
+  using namespace configuration;
+
+	volatile bool run = true;
+  std::list<std::function<void()>> ui_event_queue;
+  std::unique_ptr<abstractions::ui::AWindow> x_window(new xlib::X11_Window(views::ViewID::NONE));
+  auto* x_window_raw = x_window.get();
+
+  auto xlib_thread_function = [&ui_event_queue, x_window_raw, &run]() {
+    XInitThreads();
+    xlib::XlibWrapper::self()->create_window(configuration::Settings::get_concrete<geometry::Rectangle>(ConfigID::WINDOW_FRAME),
+        configuration::Settings::get_concrete<color::COLOR_SCHEME_TYPE>(ConfigID::WINDOW_COLOR_SCHEME),
+        configuration::Settings::get_concrete<unsigned int>(ConfigID::WINDOW_BORDER_WIDTH),
+        configuration::Settings::get_concrete<std::string>(ConfigID::WINDOW_NAME).c_str(),
+        configuration::Settings::get_concrete<std::string>(ConfigID::WINDOW_FONT_NAME).c_str());
+
+    xlib::XlibWrapper::self()->select_events_to_process();
+
+    events::EventDispatcher edispatcher;
+    XEvent event;
+    for (;run;) {
+      for(auto event_type : {Expose, KeyPress, ButtonPress, MotionNotify, ClientMessage}) {
+        bool event_is_in_queue = xlib::XlibWrapper::self()->event_in_queue(event_type, &event);
+        if(!event_is_in_queue) {
+					continue;
+				}
+        ui_event_queue.emplace_back(edispatcher.dispatch_event(x_window_raw, event));
+      }
+
+      bool queue_empty = commands::Command::xlib_queue_empty();
+      if(!queue_empty) {
+        std::unique_ptr<commands::Command> command = commands::Command::pop_xlib_command(); // DON'T USE auto HERE!
+        if(command->execute()) {
+          commands::Command::put_command_with_result(std::move(command));
+        }
+      }
+    }
+
+    xlib::XlibWrapper::self()->destroy_window();
+  };
+
+  std::thread xlib_thread(xlib_thread_function);
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1u));
+
+  commands::Command::push_xlib_command(new commands::ChangeView(views::ViewID::MENU));
+
+    try {
+      for(;run;) {
+        if(!ui_event_queue.empty()) {
+          ui_event_queue.front()();
+          ui_event_queue.pop_front();
+        }
+      }
+    } catch(...) {
+			run = false;
+    }
+
+  xlib_thread.join();
   std::cout << "GOODBYE!" << std::endl;
   return 0;
 }
