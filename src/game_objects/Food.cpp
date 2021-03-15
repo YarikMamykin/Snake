9b796bea1a9da723ce213e09dbba6b312c365193
YarikM <ymamykin@gmail.com>
2021-03-15 02:13:59 +0200

    Refactor Food with commands

--- src/game_objects/Food.cpp
+++ src/game_objects/Food.cpp
@@ -1,22 +1,16 @@
 #include "Food.hpp"
-#include "XlibWrapper.hpp"
+#include "FillCircle.hpp"
 
 namespace game_objects {
 
   Food::Food(color::Color&& background_color, color::Color&& color, geometry::Rectangle&& frame) 
   : background_color(background_color) 
   , color(color) 
-  , frame(std::move(frame)) { }
-
-  Food::~Food() {
-    xlib::XlibWrapper::self()->fill_circle(std::forward<decltype(frame)>(frame), 
-                                           std::forward<decltype(background_color)>(background_color));
-    xlib::XlibWrapper::self()->flush_buffer();
+  , frame(std::move(frame)) { 
+    commands::Command::push_xlib_command(new commands::FillCircle(frame, color));
   }
 
-  void Food::show() {
-    xlib::XlibWrapper::self()->fill_circle(std::forward<decltype(frame)>(frame), 
-                                           std::forward<decltype(background_color)>(color));
-    xlib::XlibWrapper::self()->flush_buffer();
+  Food::~Food() {
+    commands::Command::push_xlib_command(new commands::FillCircle(frame, background_color));
   }
 }
