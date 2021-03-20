e98ab1c771f539062613b91960e26aad56ea82ec
YarikM <ymamykin@gmail.com>
2021-03-20 14:55:16 +0200

    Add timer to draw Food on each Snake move

--- src/game_objects/Food.cpp
+++ src/game_objects/Food.cpp
@@ -1,16 +1,22 @@
 #include "Food.hpp"
 #include "FillCircle.hpp"
+#include "Settings.hpp"
 
 namespace game_objects {
 
   Food::Food(color::Color&& background_color, color::Color&& color, geometry::Rectangle&& frame) 
   : background_color(background_color) 
   , color(color) 
-  , frame(std::move(frame)) { 
-    commands::Command::push_xlib_command(new commands::FillCircle(frame, color));
+  , frame(std::move(frame)) {
+    timer.timeout = configuration::Settings::get_concrete<std::chrono::milliseconds>(configuration::ConfigID::SNAKE_TIMEOUT); 
+    timer.callback = [this]() {
+      commands::Command::push_xlib_command(new commands::FillCircle(this->frame, this->color));
+    };
+    timer.launch();
   }
 
   Food::~Food() {
+    timer.stop();
     commands::Command::push_xlib_command(new commands::FillCircle(frame, background_color));
   }
 }
--- src/include/game_objects/Food.hpp
+++ src/include/game_objects/Food.hpp
@@ -3,11 +3,13 @@
 
 #include "Rectangle.hpp"
 #include "Color.hpp"
+#include "Timer.hpp"
 
 namespace game_objects {
   struct Food final {
     color::Color background_color, color; 
     geometry::Rectangle frame;
+    timing::Timer timer;
 
     public:
     Food(color::Color&& background_color, color::Color&& color, geometry::Rectangle&& frame);
