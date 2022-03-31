2a2bdc96151b29e4d6ce4e8a46af244ab585a128
YarikM <ymamykin@gmail.com>
2022-04-01 00:57:28 +0300

    Rework XlibThread

--- src/threads/XlibThread.cpp
+++ src/threads/XlibThread.cpp
@@ -1,6 +1,8 @@
 #include "threads/XlibThread.hpp"
 #include "events/EventDispatcher.hpp"
-#include "xlib/XlibWrapper.hpp"
+#include <xlib/XProxy.hpp>
+#include <xlib/XSettings.hpp>
+#include <xlib/XError.hpp>
 #include "configuration/Settings.hpp"
 #include "commands/ChangeView.hpp"
 #include "xlib/X11_Window.hpp"
@@ -17,25 +19,36 @@ namespace threads {
 
   XlibThread::XlibThread(std::list<std::function<void()>>& ui_event_queue, bool& run, std::condition_variable& ui_events_available) 
   : xlib_thread(std::async(std::launch::async, [&ui_event_queue, &ui_events_available, &run, x_window = std::make_unique<xlib::X11_Window>(views::ViewID::NONE)]() {
-    XInitThreads();
 
-      using namespace configuration;
-    run = xlib::XlibWrapper::self()->create_window(
-        Settings::get_concrete<geometry::Rectangle>(ConfigID::WINDOW_FRAME),
-        Settings::get_concrete<color::COLOR_SCHEME_TYPE>(ConfigID::WINDOW_COLOR_SCHEME),
-        Settings::get_concrete_ref<unsigned int>(ConfigID::WINDOW_BORDER_WIDTH),
-        Settings::get_concrete_ref<std::string>(ConfigID::WINDOW_NAME).c_str(),
-        Settings::get_concrete_ref<std::string>(ConfigID::WINDOW_FONT_NAME).c_str());
+        auto xsettings = std::make_unique<xlib::XSettings>();
+        xsettings->font_name = configuration::Settings::get_concrete_ref<std::string>(configuration::ConfigID::WINDOW_FONT_NAME);
+        xsettings->window_name = configuration::Settings::get_concrete_ref<std::string>(configuration::ConfigID::WINDOW_NAME);
+        xsettings->border_width = configuration::Settings::get_concrete<unsigned int>(configuration::ConfigID::WINDOW_BORDER_WIDTH);
+        xsettings->window_color_scheme = configuration::Settings::get_concrete_ref<color::COLOR_SCHEME_TYPE>(configuration::ConfigID::WINDOW_COLOR_SCHEME);
 
-    xlib::XlibWrapper::self()->select_events_to_process();
+        auto& window_frame = configuration::Settings::get_concrete_ref<geometry::Rectangle>(configuration::ConfigID::WINDOW_FRAME);
+        xsettings->window_geometry = { 
+          window_frame.x,
+          window_frame.y,
+          window_frame.width,
+          window_frame.height
+        };
+            
+        auto xproxy_variant = xlib::XProxy::create(std::move(xsettings));
+
+        if(std::holds_alternative<xlib::XError>(xproxy_variant)) {
+          return;
+        }
+
+        auto& xproxy = std::get<std::reference_wrapper<xlib::XProxy>>(xproxy_variant).get();
 
     auto* x_window_raw = x_window.get();
     events::EventDispatcher edispatcher;
     XEvent event;
-    auto&& thread_sleep_timeout = config::get_concrete<std::chrono::microseconds>(config_id::THREADS_SLEEP_TIMEOUT);
+    auto thread_sleep_timeout = config::get_concrete<std::chrono::microseconds>(config_id::THREADS_SLEEP_TIMEOUT);
     for (;run;) {
       for(auto event_type : {Expose, KeyPress, ButtonPress, MotionNotify, ClientMessage}) {
-        bool event_is_in_queue = xlib::XlibWrapper::self()->event_in_queue(event_type, &event);
+        bool event_is_in_queue = xproxy.event_in_queue(event_type, &event);
         if(!event_is_in_queue) {
 					continue;
 				}
@@ -47,7 +60,7 @@ namespace threads {
       bool queue_empty = commands::Command::xlib_queue_empty();
       if(!queue_empty) {
         std::unique_ptr<commands::Command> command = commands::Command::pop_xlib_command(); // DON'T USE auto HERE!
-        command->execute();
+        command->execute(xproxy);
         continue;
       } 
 
@@ -55,7 +68,6 @@ namespace threads {
       cv.wait_for(lock, thread_sleep_timeout, [](){return true;});
     }
 
-    xlib::XlibWrapper::self()->destroy_window();
   })) { }
 
   XlibThread::~XlibThread() = default;
