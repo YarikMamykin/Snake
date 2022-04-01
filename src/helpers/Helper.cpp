766070c54ee9c3fa5dbd2781ba65dfaafba3e818
YarikM <ymamykin@gmail.com>
2022-04-01 13:41:49 +0300

    Remove XHandlers usage from Helper

--- src/helpers/Helper.cpp
+++ src/helpers/Helper.cpp
@@ -6,8 +6,6 @@ namespace helpers {
     XEvent event;
     event.xclient.type = ClientMessage; 
     event.xclient.format = 32;
-    event.xclient.window = xlib::XlibWrapper::self()->window;
-    event.xclient.display = xlib::XlibWrapper::self()->display;
     event.xclient.data.l[0] = events::AdditionalEvents::ExitApplication;
     return event;
   }
@@ -16,20 +14,18 @@ namespace helpers {
     XEvent event;
     event.xclient.type = ClientMessage; 
     event.xclient.format = 32;
-    event.xclient.window = xlib::XlibWrapper::self()->window;
-    event.xclient.display = xlib::XlibWrapper::self()->display;
     event.xclient.data.l[0] = events::AdditionalEvents::ChangeView;
     event.xclient.data.l[1] = viewID;
     return event;
   }
 
-  void Helper::SendExitApplicationEvent() {
+  void Helper::SendExitApplicationEvent(xlib::XProxy& proxy) {
     auto&& event = helpers::Helper::ConstructExitApplicationEvent();
-    xlib::XlibWrapper::self()->send_event(NoEventMask, &event);
+    proxy.send_client_event(NoEventMask, &event);
   }
 
-  void Helper::SendChangeViewEvent(const views::ViewID& viewID) {
+  void Helper::SendChangeViewEvent(xlib::XProxy& proxy, const views::ViewID& viewID) {
     auto&& event = helpers::Helper::ConstructChangeViewEvent(viewID);
-    xlib::XlibWrapper::self()->send_event(NoEventMask, &event);
+    proxy.send_client_event(NoEventMask, &event);
   }
 }
