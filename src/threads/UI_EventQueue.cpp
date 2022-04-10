26f93f38c0aa47807d54444d6d9f65b69aa61f67
YarikM <ymamykin@gmail.com>
2022-04-10 19:42:54 +0300

    Make UI_EventQueue more thread safe

--- src/threads/UI_EventQueue.cpp
+++ src/threads/UI_EventQueue.cpp
@@ -4,20 +4,21 @@
 namespace threads {
 
   std::mutex UI_EventQueue::ui_event_queue_mutex;
+  std::condition_variable UI_EventQueue::ui_event_queue_cv;
   std::list<UI_Event> UI_EventQueue::ui_event_queue;
 
   void UI_EventQueue::push(UI_Event&& event) {
     std::lock_guard<std::mutex> guard(ui_event_queue_mutex);
     ui_event_queue.push_back(std::move(event));
+    ui_event_queue_cv.notify_all();
   }
 
-  void UI_EventQueue::pop() {
-    std::lock_guard<std::mutex> guard(ui_event_queue_mutex);
+  UI_Event UI_EventQueue::wait_and_pop() {
+    std::unique_lock<std::mutex> guard(ui_event_queue_mutex);
+    ui_event_queue_cv.wait(guard, [](){ return !ui_event_queue.empty();});
 
-    if(ui_event_queue.empty())
-      return;
-
-    ui_event_queue.front()();
+    auto event = ui_event_queue.front();
     ui_event_queue.pop_front();
+    return event;
   }
 }
--- src/threads/UI_EventQueue.hpp
+++ src/threads/UI_EventQueue.hpp
@@ -2,6 +2,7 @@
 
 #include <list>
 #include <mutex>
+#include <condition_variable>
 #include "UI_Event.hpp"
 
 namespace threads {
@@ -9,10 +10,11 @@ namespace threads {
 
     private:
       static std::mutex ui_event_queue_mutex;
+      static std::condition_variable ui_event_queue_cv;
       static std::list<UI_Event> ui_event_queue;
 
     public:
       static void push(UI_Event&& event);
-      static void pop();
+      static UI_Event wait_and_pop();
   };
 }
